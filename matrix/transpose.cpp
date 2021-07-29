cdee840de111b78ca84f61464a1f6d3c33a97427
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-07-30 02:19:42 +0530

    Create transpose.cpp

---
+++ matrix/transpose.cpp
@@ -0,0 +1,26 @@
+class Solution
+{   
+    public:  
+    //Function to find transpose of a matrix.
+    void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        // code here 
+        int arr[n][n];
+        
+        for(int i = 0; i < n; i ++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                arr[i][j] = matrix[j][i];
+            }
+        }
+        
+        for(int i = 0; i < n; i ++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                matrix[i][j] = arr[i][j];
+            }
+        }
+    }
+};
