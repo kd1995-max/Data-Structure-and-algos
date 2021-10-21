4870a3bbbff5e36743484fb1a2f765996ec029e6
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-10-21 21:56:13 +0530

    Create Kth smallest element.cpp

---
+++ Heap/Kth smallest element.cpp
@@ -0,0 +1,16 @@
+int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+         priority_queue<int> pq;
+        
+        for(int i = l; i <= r; i++)
+        {
+            pq.push(arr[i]);
+            
+            if(pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+        
+        return pq.top();
+    }
