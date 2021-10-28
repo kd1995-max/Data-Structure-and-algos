8f6e8c048abe4dad13cb15c2ac28bdd47f33f02c
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-10-28 12:39:28 +0530

    Update Find Missing Number in a given range.cpp

--- Bit Manupulation/Find Missing Number in a given range.cpp
+++ Bit Manupulation/Find Missing Number in a given range.cpp
@@ -1,43 +1,16 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int FindMissing(vector<int> vec){
-    int n = vec.size();
-
-    int accu = 0;
-    int xorArray = 0;
-    for (int i = 0; i < n; i++)
+int MissingNumber(vector<int>& vec, int n) {
+        // Your code goes here
+    int accu = 1;
+    int xorArray = vec[0];
+    for (int i = 1; i < n - 1; i++)
     {
         xorArray ^= vec[i];
     }
 
-    for (int i = 1; i <= n + 1; i++)
+    for (int i = 2; i <= n; i++)
     {
         accu ^= i;
     }
     
     return (accu ^ xorArray);
-}
-
-int main(int argc, char const *argv[])
-{
-    int T;
-    cin >> T;
-    while (T--)
-    {
-        int n;
-        
-        cin >> n;
-        
-        vector<int> vec(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> vec[i];
-        }
-        
-        std::cout << "Missing number is " << FindMissing(vec) << std::endl;
     }
-    
-    return 0;
-}
