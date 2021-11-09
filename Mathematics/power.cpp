55a9dfaef558828c0eb1b329954ad09e7a21fc21
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-11-09 22:49:06 +0530

    Create power.cpp

---
+++ Mathematics/power.cpp
@@ -0,0 +1,24 @@
+
+    long long power(int N,int R)
+    {
+       //Your code here
+        if(R==0)
+        {
+            return 1;
+        }
+       if(R==1)
+       {
+           return N;
+       }
+       
+       long long temp = power(N,R/2)%mod;
+       temp=(temp*temp)%mod;
+       if(R%2==0)
+       {
+           return (temp)%mod;
+       }
+       if(R%2!=0) 
+       {
+           return (N*temp)%mod;
+       }
+    }
