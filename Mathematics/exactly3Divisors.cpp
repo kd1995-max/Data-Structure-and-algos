1bf917f282e2c424a46b8d217d93aa2bc52d8028
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-11-10 14:22:57 +0530

    Create exactly3Divisors.cpp

---
+++ Mathematics/exactly3Divisors.cpp
@@ -0,0 +1,27 @@
+ bool isPrime(int n)
+    {
+       if(n==1){
+          return false;
+       }    
+       if(n==2 || n==3){
+           return true;
+       }
+       if(n%2==0 || n%3==0){
+           return false;
+       }
+       for(int i=5; i*i<=n; i=i+6){
+           if(n%i==0 || n%(i+2)==0){
+               return false;
+           }
+       }
+       return true;
+    }
+    int exactly3Divisors(int N)
+    {  int count =0;
+        for(int i=1; i<=sqrt(N); i++){
+            if( isPrime(i)==true){
+               count ++;
+            }
+        }
+         return count;
+    }
