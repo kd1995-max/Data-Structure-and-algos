4e9c1a7b6c0cd2791d67eb915423cb3248ca183b
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-11-18 23:27:05 +0530

    Create sort.cpp

---
+++ Stack/sort.cpp
@@ -0,0 +1,32 @@
+void insertAtCorrectPosition(int x,stack<int> &s)
+{
+    if(s.size() == 0 or s.top() < x)    
+    s.push(x);
+    
+    else
+    {
+        int a = s.top();
+        s.pop();
+        insertAtCorrectPosition(x,s);
+        s.push(a);
+    }
+    
+    
+}
+
+void reverse(stack<int> &s)
+{
+    if(s.size() > 0)    
+    {
+        int x = s.top();
+        s.pop();
+        reverse(s);
+        insertAtCorrectPosition(x,s);
+    }
+}
+
+void SortedStack :: sort()
+{
+   //Your code here
+   reverse(s);
+}
