266dd6340d2725b7aab932f8a06c77de78494160
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-11-24 01:00:35 +0530

    Create naive-better.cpp

---
+++ LinkedList/Rearrange a linked list/naive-better.cpp
@@ -0,0 +1,36 @@
+void rearrangeEvenOdd(Node *head)
+    {
+       // Your Code here
+       if(!head || head->next == NULL)
+       return ;
+       
+       Node *odd = NULL;
+       Node *even = head -> next;
+       
+       int count = 0;
+       //by observation it is seen that when total 
+       //number of nodes turn out to be
+       //even then head pointer is pointing to the odd part
+       //otherwise it will be pointing to even part
+       
+       while(head and head->next)
+       {
+           count++;
+           
+           odd = head;
+           head = head->next;
+           
+           odd->next = odd->next->next;
+           
+       }
+
+      
+      if(count%2 != 0)
+      {
+          odd->next = even;
+          return;
+      }
+      
+        head->next = even;
+      
+    }
