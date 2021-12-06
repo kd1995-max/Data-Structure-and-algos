a8c2db9c35b6caa55d2b8fe54eaff64776eb6a25
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-12-06 20:06:47 +0530

    Create tour.cpp

---
+++ Queue Dequeue/tour.cpp
@@ -0,0 +1,23 @@
+int tour(petrolPump p[],int n)
+    {
+       int start = 0;
+       int requiredFuel = 0;
+       int extraFuel = 0;
+       
+       for(int i = 0; i < n; i++)
+       {
+           extraFuel += (p[i].petrol - p[i].distance);
+           
+           if(extraFuel < 0)
+           {
+               requiredFuel += extraFuel;
+               extraFuel = 0;
+               start = i +1;
+           }
+       }
+       
+       if((requiredFuel + extraFuel) > 0)
+       return start;
+       
+       return -1;
+    }
