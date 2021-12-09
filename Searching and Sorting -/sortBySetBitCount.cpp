bcc20c0b007e83a6f57b125ce6b29eee87fe7957
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-12-09 13:58:11 +0530

    Create sortBySetBitCount.cpp

---
+++ Searching and Sorting -/sortBySetBitCount.cpp
@@ -0,0 +1,10 @@
+static bool comp(int a , int b)
+    {
+        return __builtin_popcount(a) > __builtin_popcount(b);
+    }
+    
+    void sortBySetBitCount(int arr[], int n)
+    {
+        // Your code goes here
+        stable_sort(arr , arr + n, comp);
+    }
