9538c0be08d06c07e0d7fd6b0333cd4ba184fc95
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-12-09 22:53:40 +0530

    Create smallestNumber.cpp

---
+++ gready/smallestNumber.cpp
@@ -0,0 +1,43 @@
+string smallestNumber(int S, int D){
+        // code here 
+        
+        if(9*D < S)
+        return "-1";
+        
+        string ans = "";
+        
+        for(int i = D-1; i >= 0; i--)
+        {
+
+            if(S>9)            
+            {
+                ans = '9' + ans;
+                S -= 9;
+            }
+            else
+            {
+                if(i == 0)
+                {
+                    ans = to_string(S) + ans;
+                }
+                else
+                {
+                    ans = to_string(S - 1) + ans;
+                    i--;
+                    
+                    while(i > 0)
+                    {
+                        ans = '0' + ans;
+                        i--;
+                    }
+                    
+                    ans = '1' + ans;
+                    
+                    break;
+                }
+            }
+            
+        }
+        
+        return ans;
+    }
