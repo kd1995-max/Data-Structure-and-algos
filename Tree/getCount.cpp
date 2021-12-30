13f18eb764836c559d4027b03f500942bd2fea52
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2021-12-30 12:30:36 +0530

    Create getCount.cpp

---
+++ Tree/getCount.cpp
@@ -0,0 +1,15 @@
+int getCount(Node *root, int l, int h)
+{
+  // your code goes here   
+  if(!root)
+  return 0;
+  
+  if(root->data >= l and root->data <= h)
+      return 1 + getCount(root->left,l,h) + getCount(root->right,l,h);
+      
+  else if(root->data < l)
+      getCount(root->right,l,h);
+      
+  else
+     getCount(root->left,l,h);
+}
