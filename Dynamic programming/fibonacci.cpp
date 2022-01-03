06f4de5718f9d035f66595958c3ce93cb0d40664
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-01-03 17:30:54 +0530

    Create fibonacci.cpp

---
+++ Dynamic programming/fibonacci.cpp
@@ -0,0 +1,29 @@
+int fib(int n , vector<int> &dp)
+{
+    //Your code here
+    if(dp[n] == -1)
+    {
+        if(n == 0)
+        return 0;
+        if(n == 1)
+        return 1;
+        
+        int left = fib(n - 1,dp)%mod;
+        int right = fib(n - 2,dp)%mod;
+
+        int res = (left + right)%mod; 
+        
+        dp[n] = res;
+
+        return res;
+    }
+
+    return dp[n];
+}
+
+int fibonacci(int n)
+{
+    // Your code here
+    vector<int> dp(n + 1, -1);
+    return fib(n, dp);
+}   
