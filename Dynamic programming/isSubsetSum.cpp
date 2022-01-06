8c548448d0d8068744b47eddd5f5f19e18877405
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-01-07 01:10:38 +0530

    Create isSubsetSum.cpp

---
+++ Dynamic programming/isSubsetSum.cpp
@@ -0,0 +1,30 @@
+bool isSubsetSum(int N, int arr[], int sum){
+        // code here 
+        bool dp[N+1][sum+1];
+        
+        for(int i = 0;i<=N;i++)
+        {
+            for(int j = 0;j<=sum;j++)
+            {
+                if(i==0)
+                    dp[i][j]=false;
+                if(j==0)
+                    dp[i][j]=true;
+            }
+        }
+        
+        for(int i = 1;i<=N;i++)
+        {
+            for(int j = 1;j<=sum;j++)
+            {
+                
+                if(arr[i-1] > j)
+                    dp[i][j] = dp[i-1][j];
+                
+                else 
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                    
+            }
+        }
+        return dp[N][sum];
+    }
