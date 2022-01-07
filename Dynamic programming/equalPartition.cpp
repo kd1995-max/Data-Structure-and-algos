69cf4905f27f015611f90de2274f2a0aec13fd8e
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-01-07 10:32:25 +0530

    Create equalPartition.cpp

---
+++ Dynamic programming/equalPartition.cpp
@@ -0,0 +1,44 @@
+bool isSubsetSum(int N, int arr[], int sum){
+        // code here 
+        bool dp[N+1][sum+1];
+        
+        for(int i = 0;i<=N;i++)
+        {
+            for(int j = 0;j<=sum;j++)
+            {
+                if(i==0)
+                    dp[i][j]=false;
+                if(j==0)
+                    dp[i][j]=true;
+            }
+        }
+        
+        for(int i = 1;i<=N;i++)
+        {
+            for(int j = 1; j<=sum; j++)
+            {
+                
+                if(arr[i-1] > j)
+                    dp[i][j] = dp[i-1][j];
+                
+                else 
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                    
+            }
+        }
+        return dp[N][sum];
+    }
+    
+    int equalPartition(int N, int arr[])
+    {
+        // code here
+        int sum = 0;
+        
+        for(int i = 0; i < N; i++)
+        sum += arr[i];
+        
+        if(sum%2 != 0)
+        return 0;
+        
+        return isSubsetSum(N,arr,sum/2);
+    }
