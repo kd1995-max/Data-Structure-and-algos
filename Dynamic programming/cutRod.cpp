8b68bc79ed68c2f8b887801fb42cd6979b1bac62
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-01-08 23:19:33 +0530

    Create cutRod.cpp

---
+++ Dynamic programming/cutRod.cpp
@@ -0,0 +1,33 @@
+int cutRod(int price[], int N) {
+        //code here
+        int length[N];
+        
+        for(int i = 1; i <= N; i++)
+        {
+            length[ i - 1] = i;
+        }
+        
+        int dp[N + 1][N + 1];
+        
+        for(int i =0; i <= N; i++)
+        {
+            for(int j = 0; j <= N; j++)
+            {
+                if(i == 0 || j == 0)
+                dp[i][j] = 0;
+            }
+        }
+        
+        for(int i = 1; i <= N; i++)
+        {
+            for(int j = 0; j <= N; j++)
+            {
+                if(length[i - 1] > j)
+                dp[i][j] = dp[i - 1][j];
+                else
+                dp[i][j] = max(price[i - 1] + dp[i][j - length[i - 1]] , dp[i - 1][j]);
+            }
+        }
+        
+        return dp[N][N];
+    }
