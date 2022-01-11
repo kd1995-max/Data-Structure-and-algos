ad73d5ca57ada47559814f0450943b8a16c4ec60
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-01-11 17:25:43 +0530

    Create find_permutation.cpp

---
+++ String/find_permutation.cpp
@@ -0,0 +1,23 @@
+void permute(string s , int i, vector<string>& vec){
+	    if( i == s.length() - 1)
+	    {
+	        vec.push_back(s);
+	        return;
+	    }
+	    
+	    for(int j = i; j < s.size(); j++)
+	    {
+	        swap(s[i] , s[j]);
+	        permute(s , i + 1,vec);
+	        swap(s[i],s[j]);
+	    }
+	}
+		vector<string>find_permutation(string S)
+		{
+		    // Code here there
+		    int i = 0;
+	        vector<string> vec;
+	        permute(S , 0 , vec);
+	        	        sort(vec.begin() , vec.end());
+	        return vec;
+		}
