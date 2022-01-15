a9d40e6a14302e2c8f2d5211005aea27e259fe86
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-01-15 17:22:40 +0530

    Create shortestCommonSupersequence.cpp

---
+++ Dynamic programming/shortestCommonSupersequence.cpp
@@ -0,0 +1,26 @@
+    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    {
+        //code here
+        int dp[m + 1][n + 1];
+        for(int i = 0; i <= m; i++)
+        {
+            for(int j = 0; j <= n; j++)
+            {
+                if(i == 0 || j == 0)
+                dp[i][j] = 0;
+            }
+        }
+        
+        for(int i = 1; i <= m; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(X[i - 1] == Y[ j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                dp[i][j] = max(dp[i][j - 1] , dp[i - 1][j]);
+            }
+        }
+        
+        return m + n - dp[m][n];
+    }
