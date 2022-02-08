be7b8b821b86c14074c88dd14222fef9187f3d66
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-02-08 11:58:29 +0530

    Update convertToBst.cpp

--- Heap/convertToBst.cpp
+++ Heap/convertToBst.cpp
@@ -1,72 +1,28 @@
-// Create an array arr[] of size n, where n is the number of nodes in the given BST.
-// Perform the inorder traversal of the BST and copy the node values in the arr[] in sorted order.
-// Now perform the preorder traversal of the tree.
-// While traversing the root during the preorder traversal, one by one copy the values from the array arr[] to the nodes.
-
-#include<bits/stdc++.h>
-using namespace std;
-
-struct Node
+void toMinHeap(BinaryTreeNode *root, vector<int> &vec ,int &idx)
 {
-    int data;
-    Node *left, *right;
-    Node(int data)
-    {
-        this->data = data;
-        left = right = NULL;
-    }
-};
-
-void inorder(Node *root , vector<int> &v)
-{
-    if(root == NULL)
+    if(!root)
         return;
-    inorder(root->left, v);
-    v.push_back(root->data);
-    inorder(root->right, v);
+   	root->data = vec[idx++];
+    toMinHeap(root->left,vec,idx);
+    toMinHeap(root->right,vec,idx);
 }
 
-void helper(Node *root, vector<int> &v , int &idx)
+void inorder(BinaryTreeNode *root, vector<int> &vec)
 {
-    if(root == NULL)
+    if(!root)
         return;
-
-    root->data = v[idx++];
-
-    helper(root->left, v,idx);
-    helper(root->right, v,idx);
+   	
+    inorder(root->left,vec);
+    vec.push_back(root->data);
+    inorder(root->right,vec);
 }
-
-void convertToBst(Node *root,vector<int> &v)
+BinaryTreeNode* convertBST(BinaryTreeNode* root)
 {
-    inorder(root, v);
+	// Write your code here.
+    vector<int> vec;
+    inorder(root,vec);
     int idx = 0;
-    helper(root, v, idx);
-}
-
-void printTree(Node *root)
-{
-    if(root == NULL)
-        return;
-    cout<<root->data<<" ";
-    printTree(root->left);
-    printTree(root->right);
-}
-
-int main(int argc, char const *argv[])
-{
-    Node *root = new Node(4);
-    root->left = new Node(2);
-    root->right = new Node(6);
-    root->left->left = new Node(1);
-    root->left->right = new Node(3);
-    root->right->left = new Node(5);
-    root->right->right = new Node(7);   
-
-    vector<int> v;
-    convertToBst(root, v);
+    toMinHeap(root,vec,idx);
     
-    printTree(root);
-
-    return 0;
+    return root;
 }
