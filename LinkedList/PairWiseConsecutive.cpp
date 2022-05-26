310281f04a823b2493df590555bb8162f0bb9b1c
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-05-26 11:45:32 +0530

    Update PairWiseConsecutive.cpp

--- LinkedList/PairWiseConsecutive.cpp
+++ LinkedList/PairWiseConsecutive.cpp
@@ -1,30 +1,22 @@
-  struct Node *reverse (struct Node *head, int k)
-    { 
-        // Complete this method
-        if (!head)
-        return NULL;
-        
-        Node *first,*second,*third;
-        first = NULL;
-        second=third=head;
-        int count = 0;
-        while(second && count < k)
-        {
-            third = second->next;
-            second->next = first;
-            first = second;
-            second = third;
-            count++;
-        }
-        
-        if(third)
-        head->next = reverse(third,k);
-        
-        return first;
-    }
-    Node* pairWiseSwap(struct Node* head) 
+ Node* pairWiseSwap(struct Node* head) 
     {
         // The task is to complete this method
-        head = reverse(head,2);
-        return head;
+        Node* dummy = new Node(-1);
+        
+        Node* prev = dummy;
+        
+        Node* curr = head;
+        
+        dummy->next = head;
+        
+        while(curr and curr->next)
+        {
+            prev->next = curr->next;
+            curr->next = curr->next->next;
+            prev->next->next = curr;
+            curr = curr->next;
+            prev = prev->next->next;
+        }
+        
+        return dummy->next;
     }
