252da0a6bece3712e7c59d22fa05643865ddcda6
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-06-10 16:39:54 +0530

    Create transfigure.cpp

---
+++ String/transfigure.cpp
@@ -0,0 +1,77 @@
+int transfigure (string A, string B)
+
+    {
+
+    	// Your code goes here
+
+    	int check[256]={0};
+
+        if(A.length()!=B.length())
+
+        {
+
+            return -1;
+
+        }
+
+        
+
+        for(int i=0;i<A.length();i++)
+
+        {
+
+            check[A[i]]++;
+
+            check[B[i]]--;
+
+        }
+
+        
+
+        for(int i=0;i<256;i++)
+
+        {
+
+            if(check[i]!=0)
+
+            return -1;
+
+        }
+
+        
+
+        int len1=A.length();
+
+        int len2=B.length();
+
+        int res=0;
+
+        
+
+        while(len1>=0)
+
+        {
+
+            if(A[len1]!=B[len2])
+
+            {
+
+                res++;
+
+            }
+
+            else
+
+            {
+
+                len2--;
+
+            }
+
+            len1--;
+
+        }
+
+        return res;
+
+    }
