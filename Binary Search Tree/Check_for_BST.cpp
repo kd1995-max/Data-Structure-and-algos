a7006aa8322a03daaf6e3068de3b92f0cd19da9e
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-06-16 19:28:29 +0530

    Update Check_for_BST.cpp

--- Binary Search Tree/Check_for_BST.cpp
+++ Binary Search Tree/Check_for_BST.cpp
@@ -15,3 +15,25 @@
         // You(r code here
         return isBST_(root);
     }
+
+// method 2
+bool solve(Node* root,Node* min = NULL,Node* max = NULL)
+    {
+        if(!root)    
+        return true;
+        else if(min and min->data >= root->data)
+        return false;
+        else if(max and max->data <= root->data)
+        return false;
+        
+        bool left = solve(root->left,min,root);
+        bool right = solve(root->right,root,max);
+        
+        return left and right;
+    }
+    
+    bool isBST(Node* root) 
+    {
+        // Your code here
+        return solve(root,NULL,NULL);
+    }
