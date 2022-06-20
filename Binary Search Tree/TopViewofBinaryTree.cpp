db1eda175839df173174fbb3ef8338519c1e89c7
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-06-20 18:12:45 +0530

    Update TopViewofBinaryTree.cpp

--- Binary Search Tree/TopViewofBinaryTree.cpp
+++ Binary Search Tree/TopViewofBinaryTree.cpp
@@ -1,41 +1,33 @@
-class Solution
-{
-    public:
-    //Function to return a list of nodes visible from the top view 
-    //from left to right in Binary Tree.
-    
-    void top(Node *root ,map<int,int> &m)
-    {
-        queue<pair<Node*,int>>q;
-        q.push({root, 0});
-        while(q.empty() == false){
-            
-            auto p = q.front();
-            Node *curr = p.first;
-            q.pop();
-            
-            int hd = p.second;
-            
-            if(m.find(hd) == m.end())
-            m[hd] = curr->data;
-            
-            if(curr->left!=NULL)
-            q.push({curr->left,hd-1});
-            if(curr->right!=NULL)
-            q.push({curr->right,hd+1});
-        }
-    }
-    vector<int> topView(Node *root)
+ vector<int> topView(Node *root)
     {
         //Your code here
-        vector<int> vec;
-        map<int,int> m;
-        top(root , m);
+        map<int,int> mp;
         
-        for(auto a : m)
-        vec.push_back(a.second);
+        queue<pair<Node*,int>> q;
+        q.push({root,0});
+        
+        while(!q.empty())
+        {
+            Node* curr = q.front().first;
+            int num = q.front().second;
+            
+            if(mp.find(num) == mp.end())
+            mp[num] = curr->data;
+            
+            q.pop();
+            
+            if(curr->left)
+            q.push({curr->left,num - 1});
+            
+            if(curr->right)
+            q.push({curr->right,num + 1});
+        }
+        vector<int> vec;
+        
+        for(auto a : mp)
+        {
+            vec.push_back(a.second);
+        }
         
         return vec;
     }
-
-};
