cda19ef8795cad9753c46a81c7e01ef53680970d
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-02 01:27:27 +0530

    Update topoSort.cpp

--- graph/topoSort.cpp
+++ graph/topoSort.cpp
@@ -1,3 +1,4 @@
+// DFS
 void solve(int node, stack<int> &ans, vector<int> &vis, vector<int> adj[])
 	{
 	    vis[node] = true;
@@ -35,4 +36,46 @@ void solve(int node, stack<int> &ans, vector<int> &vis, vector<int> adj[])
 	    }
 	    
 	    return res;
+	}
+// BFS - Kahan's Algorithm
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int> indegree(V + 1 , 0);
+	    
+	    for(int i = 0; i < V; i++)
+	    {
+	        for(auto it : adj[i])
+	        {
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    for(int i = 0; i < V; i++)
+	    {
+	        if(indegree[i] == 0)
+	        {
+	            q.push(i);
+	        }
+	    }
+	    
+	    vector<int> topo;
+	    while(!q.empty())
+	    {
+	        int node = q.front();
+	        q.pop();
+	        
+	        topo.push_back(node);
+	        
+	        for(auto it : adj[node])
+	        {
+	            indegree[it]--;
+	            
+	            if(indegree[it] == 0)
+	            q.push(it);
+	        }
+	    }
+	    
+	    return topo;
 	}
