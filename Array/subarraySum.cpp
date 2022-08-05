e8e2ad5a9b6669c340d310e15c5ca83f6711f1f8
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-05 13:45:31 +0530

    Update subarraySum.cpp

--- Array/subarraySum.cpp
+++ Array/subarraySum.cpp
@@ -1,19 +1,15 @@
 vector<int> subarraySum(int arr[], int n, int s)
     {
         if(n == 1 && arr[0] == s)
-        {
-        	return {1 , 1};
-        }
-        
+        return {1 , 1};
+	
         else if(n == 1 && arr[0] != s)
         return {-1};
  
-		int sum = arr[0] , start = 0 , end;
-        vector<int> vec;
- 
+	int sum = arr[0] , start = 0 , end;
+         
         for ( end = 1; end <= n; end++)
         {
-
             while(sum > s && start < end - 1)
             {
             	sum -= arr[start];
@@ -27,7 +23,6 @@ vector<int> subarraySum(int arr[], int n, int s)
 
             if(end < n)
             sum += arr[end];
-
         }
         return {-1};
     }
