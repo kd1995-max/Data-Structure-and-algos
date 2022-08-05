652fcdf4607ad4df27490b64fb4f1b7dd3655a58
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-05 23:08:04 +0530

    Create eventualSafeNodes.cpp

---
+++ graph/eventualSafeNodes.cpp
@@ -0,0 +1,45 @@
+bool dfs(int node, vector<bool> &vis,vector<bool> &dfsVis,vector<vector<int>>& graph,vector<bool> &cycle)
+    {
+        vis[node] = true;
+        dfsVis[node] = true;
+        
+        for(auto nbr : graph[node])
+        {
+            if(!vis[nbr])
+            {
+                if(dfs(nbr,vis,dfsVis,graph,cycle))
+                    return cycle[node] = true;
+            }
+            else if(vis[nbr] and dfsVis[nbr])
+                return cycle[node] = true;
+        }
+        
+        return dfsVis[node] = false;
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        
+        vector<bool> vis(n , false);
+        vector<bool> dfsVis(n , false);
+        vector<bool> cycle(n , false);
+        for(int i = 0; i < n; i++)
+        {
+            if(!vis[i])
+            {
+                dfs(i,vis,dfsVis,graph,cycle);
+            }
+        }
+        
+        vector<int> ans;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(!cycle[i])
+            {
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
