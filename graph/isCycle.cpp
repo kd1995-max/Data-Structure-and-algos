c712e05b1b2b004c544427b33c216af060377bc7
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-07 18:42:00 +0530

    Update isCycle.cpp

--- graph/isCycle.cpp
+++ graph/isCycle.cpp
@@ -1,3 +1,4 @@
+// BFS
 bool checkForCycle(int s, int v,vector<int> adj[], vector<int>&visited)
     {
         queue<pair<int,int>>q;
@@ -35,6 +36,41 @@ bool checkForCycle(int s, int v,vector<int> adj[], vector<int>&visited)
                 if(checkForCycle(i , V, adj, vis))
                 return true;
             }
+        }
+        
+        return false;
+    }
+
+// DFS
+    bool solve(int node, int parent,vector<bool> &vis, vector<int> adj[])
+    {
+        vis[node] = true;
+        
+        for(auto it : adj[node])
+        {
+            if(!vis[it])
+            {
+                if(solve(it,node,vis,adj))
+                return true;
+            }
+            else if(it != parent)
+            return true;
+        }
+        
+        return false;
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        vector<bool> vis(V + 1 , 0);
+        
+        for(int i = 0; i < V; i++)
+        {
+            if(!vis[i])
+            {
+                if(solve(i,-1,vis,adj))    
+                return true;
+            }
+            
         }
         
         return false;
