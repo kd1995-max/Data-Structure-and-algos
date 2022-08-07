96053ca0f8a11f074636e4eed083c86e4529a72a
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-08 02:19:13 +0530

    Create tarjans.cpp

---
+++ graph/tarjans.cpp
@@ -0,0 +1,61 @@
+    vector<vector<int> >ans;
+   void DFS(int u,vector<int>& disc,vector<int>& low,stack<int>& mystack,vector<bool>& presentInStack,vector<int> adj[])
+   {
+    static int time = 0;
+    disc[u] = low[u] = time;
+    time++;
+    mystack.push(u);
+    presentInStack[u] = true;
+   
+    for(int v: adj[u])
+    {
+     if(disc[v]==-1) 
+     {
+      DFS(v,disc,low,mystack,presentInStack,adj);
+      low[u] = min(low[u],low[v]);
+     }
+     else if(presentInStack[v]) 
+      low[u] = min(low[u],disc[v]);
+    }
+   
+    if(low[u]==disc[u]) 
+    {
+        vector<int>temp;
+        
+         while(mystack.top()!=u)
+         {
+              temp.push_back(mystack.top());
+              presentInStack[mystack.top()] = false;
+              mystack.pop();
+         }
+         
+         temp.push_back(mystack.top());
+         
+         presentInStack[mystack.top()] = false;
+         
+         mystack.pop();
+         
+         sort(temp.begin(),temp.end());
+         
+      ans.push_back(temp);
+    }
+   }
+   
+   vector<vector<int>> tarjans(int V, vector<int> adj[])
+   {
+        vector<int> disc(V,-1),low(V,-1);
+        vector<bool> presentInStack(V,false); 
+        stack<int> mystack;
+    
+        for(int i=0;i<V;++i)
+        {
+             if(disc[i]==-1)
+             {
+                DFS(i,disc,low,mystack,presentInStack,adj);
+             }
+        }
+        
+        sort(ans.begin(),ans.end());
+        
+        return ans;
+   }
