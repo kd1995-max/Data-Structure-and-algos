528b516b5c9db4cc6aa5b806eeecf3be4c8556b5
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-17 20:07:21 +0530

    Update FloydsWarshal.cpp

--- graph/FloydsWarshal.cpp
+++ graph/FloydsWarshal.cpp
@@ -1,78 +1,28 @@
-	void shortest_distance(vector<vector<int>>&m){
+	void shortest_distance(vector<vector<int>>& mat){
 	    // Code here
-	   int n=m.size();
-	    for(int k=0;k<n;k++)
-	    {
-	        for(int i=0;i<n;i++)
-	        {
-	            for(int j=0;j<n;j++)
-	            {
-	                if(m[i][k]==-1||m[k][j]==-1) 
-	                continue;
-	                else if(m[i][j]==-1) 
-	                m[i][j]=m[i][k]+m[k][j];
-	                else 
-	                m[i][j]=min(m[i][j],m[i][k]+m[k][j]);
+	    int n = mat.size();
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < n; j++){
+	            if(mat[i][j]==-1) mat[i][j] = INT_MAX;
+	        }
+	    }
+	    
+	    for(int k = 0; k < n; k++){
+	        for(int i = 0; i < n; i++){
+	            for(int j = 0; j < n; j++){
+	            
+	                if(mat[i][k]!=INT_MAX && mat[k][j]!=INT_MAX && mat[i][k]+mat[k][j]<mat[i][j]){
+	                    mat[i][j] = mat[i][k]+mat[k][j];
+	                }
+	            
 	            }
 	        }
 	    }
+	    
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < n; j++){
+	            if(mat[i][j]==INT_MAX) mat[i][j] = -1;
+	        }
+	    }
+	    
 	}
-  
-  //Video Code
-  #include<bits/stdc++.h>
-using namespace std;
-
-#define V 6		//No of vertices
-
-void floyd_warshall(int graph[V][V])
-{
-	int dist[V][V];
-
-	//Assign all values of graph to allPairs_SP
-	for(int i=0;i<V;++i)
-		for(int j=0;j<V;++j)
-			dist[i][j] = graph[i][j];
-
-	//Find all pairs shortest path by trying all possible paths
-	for(int k=0;k<V;++k)	//Try all intermediate nodes
-		for(int i=0;i<V;++i)	//Try for all possible starting position
-			for(int j=0;j<V;++j)	//Try for all possible ending position
-			{
-				if(dist[i][k]==INT_MAX || dist[k][j]==INT_MAX)	//SKIP if K is unreachable from i or j is unreachable from k
-					continue;
-				else if(dist[i][k]+dist[k][j] < dist[i][j])		//Check if new distance is shorter via vertex K
-					dist[i][j] = dist[i][k] + dist[k][j];
-			}
-
-	//Check for negative edge weight cycle
-	for(int i=0;i<V;++i)
-		if(dist[i][i] < 0)
-		{
-			cout<<"Negative edge weight cycle is present\n";
-			return;
-		}
-
-	//Print Shortest Path Graph
-	//(Values printed as INT_MAX defines there is no path)
-	for(int i=1;i<V;++i)
-	{
-		for(int j=0;j<V;++j)
-			cout<<i<<" to "<<j<<" distance is "<<dist[i][j]<<"\n";
-		cout<<"=================================\n";
-	}
-}
-
-int main()
-{
-	int graph[V][V] = { {0, 1, 4, INT_MAX, INT_MAX, INT_MAX},
-						{INT_MAX, 0, 4, 2, 7, INT_MAX},
-						{INT_MAX, INT_MAX, 0, 3, 4, INT_MAX},
-						{INT_MAX, INT_MAX, INT_MAX, 0, INT_MAX, 4},
-						{INT_MAX, INT_MAX, INT_MAX, 3, 0, INT_MAX},
-						{INT_MAX, INT_MAX, INT_MAX, INT_MAX, 5, 0} };
-
-	floyd_warshall(graph);
-	return 0;
-}
-
-//TIME COMPLEXITY: O(V^3)
