ab592274b120d418ac35393d5dc90d92df54f562
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-19 20:17:23 +0530

    Update maxAreaOfIsland.cpp

--- graph/maxAreaOfIsland.cpp
+++ graph/maxAreaOfIsland.cpp
@@ -66,7 +66,7 @@ bool isValid(int i , int j,vector<vector<int>> &mat,int n,int m)
         return islands;
     }
 
-// GFG
+// GFG - OLD
 bool isValid(int i , int j,vector<vector<int>> &mat,int n,int m)
     {
         if(i >= 0 and i < n and j >= 0 and j < m and mat[i][j] == 1)
@@ -154,3 +154,46 @@ bool isValid(int i , int j,vector<vector<int>> &mat,int n,int m)
         
         return islands;
     }
+// =================
+// GFG New
+
+void dfs(vector<vector<int>>& grid, int i , int j, int rows, int cols, int &area) 
+{
+        if(i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == 0)
+        return;
+            
+        grid[i][j] = 0; // visited
+        
+        ++area;
+        
+        dfs(grid, i + 1, j, rows, cols, area);
+        dfs(grid, i - 1, j, rows, cols, area);
+        dfs(grid, i, j + 1, rows, cols, area);
+        dfs(grid, i, j - 1, rows, cols, area);
+        dfs(grid, i + 1, j + 1, rows, cols, area);
+        dfs(grid, i - 1, j + 1, rows, cols, area);
+        dfs(grid, i + 1, j - 1, rows, cols, area);
+        dfs(grid, i - 1, j - 1, rows, cols, area);
+    }
+    
+int findMaxArea(vector<vector<int>>& grid) 
+{
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        int mxArea = 0;
+        
+        for(int i = 0; i < rows; ++i) 
+        {
+            for(int j = 0; j < cols; ++j) 
+            {
+                if(grid[i][j] == 1) 
+                {
+                    int area = 0;
+                    dfs(grid, i, j, rows, cols, area);
+                    mxArea = max(mxArea, area);
+                }
+            }
+        } 
+        return mxArea;
+}
