6321d56ce3eddd9c06feb08f9ca8440690c375d5
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-26 04:17:25 +0530

    Create Best Time to Buy and Sell Stock with Transaction Fee.cpp

---
+++ Dynamic programming/Striver Dp series/Best Time to Buy and Sell Stock with Transaction Fee/Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& Arr, int fee) {
+        int n = Arr.size();
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+    
+        for(int ind = n-1; ind>=0; ind--){
+            for(int buy=0; buy<=1; buy++){
+                int profit;
+
+                if(buy==1)
+                {// We can buy the stock
+                    profit = max(0+dp[ind+1][1], -Arr[ind] + dp[ind+1][0]);
+                }
+                else
+                {// We can sell the stock
+                    profit = max(0+dp[ind+1][0], Arr[ind] + dp[ind+1][1] - fee);
+                }
+
+                dp[ind][buy] = profit;
+            }
+        }
+
+        return dp[0][1];
+    }
+};
