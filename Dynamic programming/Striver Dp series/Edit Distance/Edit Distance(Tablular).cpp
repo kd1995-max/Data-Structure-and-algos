7233976ebb0b41fb8b2c9ea3bd2f013f75214247
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-25 19:37:34 +0530

    Create Edit Distance(Tablular).cpp

---
+++ Dynamic programming/Striver Dp series/Edit Distance/Edit Distance(Tablular).cpp
@@ -0,0 +1,25 @@
+ int editDistance(string S1, string S2) {
+        // Code here
+            int n = S1.size();
+            int m = S2.size();
+            
+            vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+            
+            for(int i=0;i<=n;i++){
+                dp[i][0] = i;
+            }
+            for(int j=0;j<=m;j++){
+                dp[0][j] = j;
+            }
+            
+            for(int i=1;i<n+1;i++){
+                for(int j=1;j<m+1;j++){
+                    if(S1[i-1]==S2[j-1])
+                        dp[i][j] = 0+dp[i-1][j-1];
+                    
+                    else dp[i][j] = 1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+            
+            return dp[n][m];
+    }
