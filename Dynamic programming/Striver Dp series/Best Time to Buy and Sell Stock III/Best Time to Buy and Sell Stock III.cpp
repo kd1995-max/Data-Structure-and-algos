3daffd6d1c77d650fb5edde7a4bd1b17ea2ad6d3
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-08-26 03:10:54 +0530

    Update Best Time to Buy and Sell Stock III.cpp

--- Dynamic programming/Striver Dp series/Best Time to Buy and Sell Stock III/Best Time to Buy and Sell Stock III.cpp
+++ Dynamic programming/Striver Dp series/Best Time to Buy and Sell Stock III/Best Time to Buy and Sell Stock III.cpp
@@ -35,3 +35,31 @@ public:
         return solve(0,1,values,n,dp,2);
     }
 };
+
+// GFG Tabularized
+int maxProfit(vector<int>&Arr){
+    //Write your code here..
+     int n = Arr.size();
+             vector<vector<vector<int>>> dp(n+1,
+                                    vector<vector<int>> 
+                                            (2,vector<int>(3,0)));
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy = 0; buy<=1; buy++){
+            for(int cap=1; cap<=2; cap++){
+                
+                if(buy==1){
+                    dp[ind][buy][cap] = max(0+dp[ind+1][1][cap], 
+                                -Arr[ind] + dp[ind+1][0][cap]);
+                }
+                else{
+                    dp[ind][buy][cap] = max(0+dp[ind+1][0][cap],
+                                Arr[ind] + dp[ind+1][1][cap - 1]);
+                }
+            }
+        }
+    }
+    
+    
+    return dp[0][1][2];
+}
