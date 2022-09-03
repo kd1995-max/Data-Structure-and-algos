83ab8072e0855f6c61ca8ec248fa31c003a97967
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-09-03 14:46:16 +0530

    Update recursive.cpp

--- Dynamic programming/Minimum number of jumps to reach end/recursive.cpp
+++ Dynamic programming/Minimum number of jumps to reach end/recursive.cpp
@@ -1,21 +1,29 @@
-    int solve(int i , int arr[], int n)
+int jump(int nums[], int pos,int n) 
     {
-        if(i == n - 1)    
-        return 0;
-        
-        int ans = INT_MAX - 5;
-        
-        for(int s = 1; i < n && s <= arr[i]; s++)
-        {
-            int jump = solve(s + i, arr, n) + 1;
-            
-            ans = min(ans , jump);
-        }
-        
-        return ans;
-    }
+	if(pos >= n - 1) return 0;        
+	int minJumps = 1e9;  // initialising to max possible jumps + 1
+	
+	for(int j = 1; j <= nums[pos]; j++)  // explore all possible jump sizes from current position
+		{
+		    int jumps = jump(nums, pos + j,n);
+		    minJumps = min(minJumps, 1 + jumps);
+		}
+	return minJumps;
+}
     
     int minJumps(int arr[], int n){
         // Your code here
-        return solve(0,arr,n);
+        if(n == 1)
+        return 0;
+        
+        if(arr[0] == 0)
+        return -1;
+        
+        int ans = jump(arr,0,n);
+        
+        if(ans == 1e9)
+        return -1;
+        
+        else 
+        return ans;
     }
