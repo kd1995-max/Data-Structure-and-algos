56cee4ccd1526937c4c854febf013b2685ce8a93
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-09-04 02:07:10 +0530

    Create Permutation Coefficient.cpp

---
+++ Dynamic programming/Permutation Coefficient.cpp
@@ -0,0 +1,64 @@
+//Tabular
+// A O(n) solution that uses
+// table fact[] to calculate
+// the Permutation Coefficient
+#include<bits/stdc++.h>
+using namespace std;
+
+// Returns value of Permutation
+// Coefficient P(n, k)
+int permutationCoeff(int n, int k)
+{
+	int fact[n + 1];
+
+	// Base case
+	fact[0] = 1;
+
+	// Calculate value
+	// factorials up to n
+	for(int i = 1; i <= n; i++)
+	fact[i] = i * fact[i - 1];
+
+	// P(n,k) = n! / (n - k)!
+	return fact[n] / fact[n - k];
+}
+
+// Driver Code
+int main()
+{
+	int n = 10, k = 2;
+	
+	cout << "Value of P(" << n << ", "
+		<< k << ") is "
+		<< permutationCoeff(n, k);
+
+	return 0;
+}
+
+//Optimized
+// A O(n) time and O(1) extra
+// space solution to calculate
+// the Permutation Coefficient
+#include <iostream>
+using namespace std;
+
+int PermutationCoeff(int n, int k)
+{
+	int P = 1;
+
+	// Compute n*(n-1)*(n-2)....(n-k+1)
+	for (int i = 0; i < k; i++)
+		P *= (n-i) ;
+
+	return P;
+}
+
+// Driver Code
+int main()
+{
+	int n = 10, k = 2;
+	cout << "Value of P(" << n << ", " << k
+		<< ") is " << PermutationCoeff(n, k);
+
+	return 0;
+}
