bedc05a1bd6c93ae3b76cbb965e831156c3b04c1
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-09-04 05:56:43 +0530

    Create tabulation.cpp

---
+++ Dynamic programming/Bell Number/tabulation.cpp
@@ -0,0 +1,23 @@
+    int bellNumber(int n)
+    {
+       // Code Here
+       long long dp[n+1][n+1];
+       dp[0][0]=1;
+       
+       long long MOD = 1000000007;
+       
+       for(int i=1;i<=n;i++) {
+           
+           dp[i][0]=dp[i-1][i-1];
+           
+           for(int j=1;j<=i;j++) {
+               
+               dp[i][j] = ((dp[i][j-1]%MOD)+(dp[i-1][j-1]%MOD))%MOD;
+               
+           }
+           
+       }
+       
+       return dp[n][0];
+       
+    }
