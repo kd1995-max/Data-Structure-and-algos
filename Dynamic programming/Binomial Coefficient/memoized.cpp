c8d5ef6618693cc757b3a9d2ed03b474c8f6118d
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-09-05 17:17:11 +0530

    Update memoized.cpp

--- Dynamic programming/Binomial Coefficient/memoized.cpp
+++ Dynamic programming/Binomial Coefficient/memoized.cpp
@@ -1,21 +1,36 @@
 int mod = 1e9 + 7;
-    
-    int solve(int n, int r, vector<vector<int>> &dp)
-    {
-        if(r > n)    
-        return 0;
-        
-        if(r == 0 || r == n)
-        return 1;
-        
-        if(dp[n][r] != -1)
-        return dp[n][r];
-        
-        return dp[n][r] = (solve(n - 1, r - 1,dp)%mod + solve(n - 1, r,dp)%mod)%mod;
-    }
-    
-    int nCr(int n, int r){
-        // code here
-        vector<vector<int>> dp(n + 1, vector<int>(r+1,-1));
-        return solve(n,r,dp);
-    }
+class Solution{
+public:
+   int solve(int n,int r,vector<vector<int>> &dp){
+       // base case
+       if(n < r){
+           return 0;
+       }
+       
+       if(r == 0 || r == n){
+           return 1;
+       }
+       
+       if(dp[n][r] != -1){
+           return dp[n][r];
+       }
+       // NCR means selecting the r objects from n objects
+       
+       // case 1 : we selected the 1 object from the n object than
+       int taken = solve(n-1,r-1,dp);
+       
+       // case 2 : we leave the 1st object form the n object then
+       int notTaken = solve(n-1,r,dp);
+       
+       dp[n][r] = (taken + notTaken) % mod;
+       
+       return dp[n][r];
+       
+   }
+   
+
+   int nCr(int n, int r){
+       vector<vector<int>> dp(n+1,vector<int>(r+1,-1));
+       return solve(n,r,dp);
+   }
+};
