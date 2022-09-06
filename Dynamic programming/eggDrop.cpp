87850e30e66402b913ca421da87aa8605ee6bae9
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-09-06 07:29:09 +0530

    Update eggDrop.cpp

--- Dynamic programming/eggDrop.cpp
+++ Dynamic programming/eggDrop.cpp
@@ -26,3 +26,56 @@
         vector<vector<int>> dp(n + 1, vector<int>(k + 1, -1));
         return solve(n,k,dp);
     }
+
+// Optimization on above code
+    int solve(int e , int f,vector<vector<int>> &dp)
+    {
+        if(f == 0 || f == 1)
+        return f;
+        
+        if(e == 1)
+        return f;
+        
+        if(dp[e][f] != -1)
+        return dp[e][f];
+        
+        int mini = INT_MAX;
+        
+        for(int k = 1; k <= f; k++)
+        {
+            int low;
+            int high;
+            
+            if(dp[e-1][k-1] != -1)
+            {
+                low = dp[e-1][k-1];
+            }
+            else
+            {
+                low = solve(e-1,k-1,dp);
+                dp[e-1][k-1] = low;
+            }
+            
+            if(dp[e][f-k] != -1)
+            {
+                high = dp[e][f-k];
+            }
+            else
+            {
+                high = solve(e,f-k,dp);
+                dp[e][f-k] = high;
+            }
+            
+            int temp = 1 + max(low,high);
+            
+            mini = min(mini,temp);
+        }
+        
+        return dp[e][f] = mini;
+    }
+    int eggDrop(int n, int k) 
+    {
+        // your code here
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, -1));
+        return solve(n,k,dp);
+    }
