9a9d4ba8c2d10794f54e8259fc7e76d68e294cc4
krishanu dev <75378214+kd1995-max@users.noreply.github.com>
2023-10-29 12:22:38 +0530

    Update equilibriumPoint.cpp

--- Array/equilibriumPoint.cpp
+++ Array/equilibriumPoint.cpp
@@ -1,23 +1,13 @@
-int equilibriumPoint(long long a[], int n) 
-    {
-        // Your code here
-        int total_sum = 0;
-        
-        for(int i = 0; i < n; i++)
-        {
-            total_sum += a[i];
-        }
+int equilibriumPoint(long long a[], int n) {
+    
+       int total_sum = accumulate(a, a+n, 0);
         
         int pre_sum = 0;
-        for(int i = 0; i < n; i++)
-        {
-            int diff = abs(total_sum - a[i]);
-            total_sum -= a[i];
-            
-            if(diff == pre_sum)
+        for(int i = 0; i < n; i++) {
+        if(pre_sum == total_sum - pre_sum - a[i]) {
             return i + 1;
-            
-            pre_sum += a[i];
+        }
+        pre_sum += a[i];
         }
         
         return -1;
